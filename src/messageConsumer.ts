60d88e2c434011086c06ad5cc36721e125d54699
Maike Negreiros <maike@alphaomega.agency>
2023-07-24 14:44:28 -0300

    Change PR message

--- src/messageConsumer.ts
+++ src/messageConsumer.ts
@@ -2,7 +2,7 @@ import { SQSEvent } from "aws-lambda";
 
 const handler = async (event: SQSEvent) => {
   for (const record of event.Records) {
-    console.log('reading message:', record.body)
+    console.log('reading message: ', record.body)
   }
 };
 
